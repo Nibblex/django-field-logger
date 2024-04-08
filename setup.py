662e4062b1716e85ff4f320567fc7ed715d1bdb1
Sergio Rodriguez <sergio_mu12@hotmail.com>
2024-04-08 11:41:51 -0300

    docs: version update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setuptools.setup(
     name="django-field-logger",
-    version="0.0.17",
+    version="0.0.18",
     author="Sergio Rodríguez",
     author_email="srodriguez3441@gmail.com",
     description="""A Django app for logging changes in model fields.""",
