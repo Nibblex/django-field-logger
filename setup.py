83389e54fa8816ea6efa2c75135022bf96844d95
Sergio Rodriguez <sergio_mu12@hotmail.com>
2024-04-11 04:56:57 -0300

    fix: fixed long_description in setuptools.setup

--- setup.py
+++ setup.py
@@ -7,19 +7,21 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setuptools.setup(
     name="django-field-logger",
-    version="0.0.21",
+    version="0.0.22",
     description="""A Django app for logging changes in model fields.""",
-    long_description="file: README.md, LICENSE",
+    long_description=open("README.md").read(),
     author="Sergio Rodríguez",
     author_email="srodriguez3441@gmail.com",
+    maintainer="Sergio Rodríguez",
+    maintainer_email="srodriguez3441@gmail.com",
     url="https://github.com/nibblex/django-field-logger",
     packages=setuptools.find_packages(),
-    license="MIT",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    license="MIT",
     install_requires=[
         "Django>=4.0",
     ],
