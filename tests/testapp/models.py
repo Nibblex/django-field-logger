8cbe6007a45c460afd2060ee4fa6ec8e35ead832
Sergio Rodriguez <sergio_mu12@hotmail.com>
2024-07-16 03:35:19 -0300

    feat: added test_utils

--- tests/testapp/models.py
+++ tests/testapp/models.py
@@ -4,6 +4,24 @@ from fieldlogger.managers import FieldLoggerManager
 from fieldlogger.mixins import FieldLoggerMixin
 
 
+class TestModelRelated2(FieldLoggerMixin, models.Model):
+    test_char_field = models.CharField(max_length=255, null=True)
+    test_related_field3 = models.ForeignKey(
+        "TestModel", on_delete=models.CASCADE, null=True
+    )
+
+    __test__ = False
+
+
+class TestModelRelated(FieldLoggerMixin, models.Model):
+    test_char_field = models.CharField(max_length=255, null=True)
+    test_related_field2 = models.ForeignKey(
+        TestModelRelated2, on_delete=models.CASCADE, null=True
+    )
+
+    __test__ = False
+
+
 class TestModel(FieldLoggerMixin, models.Model):
     test_big_integer_field = models.BigIntegerField(null=True)
     test_binary_field = models.BinaryField(null=True)
@@ -32,7 +50,9 @@ class TestModel(FieldLoggerMixin, models.Model):
     test_time_field = models.TimeField(null=True)
     test_url_field = models.URLField(null=True)
     test_uuid_field = models.UUIDField(null=True)
-    test_foreign_key = models.ForeignKey("self", on_delete=models.CASCADE, null=True)
+    test_related_field = models.ForeignKey(
+        TestModelRelated, on_delete=models.CASCADE, null=True
+    )
 
     __test__ = False
 
--- tests/tests.py
+++ tests/tests.py
@@ -8,34 +8,35 @@ from django.conf import settings
 from fieldlogger import config, signals
 
 from .helpers import CREATE_FORM, UPDATE_FORM, check_logs, set_attributes, set_config
-from .testapp.models import TestModel
+from .testapp.models import TestModel, TestModelRelated
 
 ORIGINAL_SETTINGS = deepcopy(settings.FIELD_LOGGER_SETTINGS)
 
 
 @pytest.fixture
 def test_instance():
-    # Create two instances for the foreign key field
-    CREATE_FORM["test_foreign_key"] = TestModel.objects.create()
-    UPDATE_FORM["test_foreign_key"] = TestModel.objects.create()
+    related_instance = TestModelRelated.objects.create()
 
-    # Create the main instance
-    yield TestModel.objects.create(**CREATE_FORM)
+    UPDATE_FORM["test_related_field"] = TestModelRelated.objects.create()
+
+    return TestModel.objects.create(test_related_field=related_instance, **CREATE_FORM)
 
 
 @pytest.mark.django_db
-class TestCase:
-    def test_log_on_create(self, test_instance):
-        check_logs(test_instance, expected_count=len(CREATE_FORM), created=True)
+class TestCase1:
+    def test_log_on_direct_fields(self, test_instance):
+        check_logs(test_instance, expected_count=len(CREATE_FORM) + 1, created=True)
 
-    def test_log_on_save(self, test_instance):
-        set_attributes(test_instance, UPDATE_FORM)
-        check_logs(test_instance, expected_count=len(CREATE_FORM) + len(UPDATE_FORM))
+    @pytest.mark.parametrize("update_fields", [False, True])
+    def test_log_on_save(self, test_instance, update_fields):
+        set_attributes(test_instance, UPDATE_FORM, update_fields)
+        check_logs(test_instance, expected_count=len(CREATE_FORM) + len(UPDATE_FORM) + 1)
 
-    def test_log_on_save_twice(self, test_instance):
-        set_attributes(test_instance, UPDATE_FORM, update_fields=True)
-        set_attributes(test_instance, UPDATE_FORM)
-        check_logs(test_instance, expected_count=len(CREATE_FORM) + len(UPDATE_FORM))
+    @pytest.mark.parametrize("update_fields", [False, True])
+    def test_log_on_save_twice(self, test_instance, update_fields):
+        set_attributes(test_instance, UPDATE_FORM, update_fields)
+        set_attributes(test_instance, UPDATE_FORM, update_fields)
+        check_logs(test_instance, expected_count=len(CREATE_FORM) + len(UPDATE_FORM) + 1)
 
 
 @pytest.fixture
@@ -48,14 +49,13 @@ def restore_settings():
 
 @pytest.mark.django_db
 @pytest.mark.usefixtures("restore_settings")
+@pytest.mark.parametrize("scope", ["global", "testapp", "testmodel"])
 class TestCase2:
-    @pytest.mark.parametrize("scope", ["global", "testapp", "testmodel"])
     def test_logging_disabled(self, scope):
         set_config({"logging_enabled": False}, scope)
         test_instance = TestModel.objects.create(**CREATE_FORM)
         check_logs(test_instance, expected_count=0, created=True)
 
-    @pytest.mark.parametrize("scope", ["global", "testapp", "testmodel"])
     def test_fail_silently(self, scope):
         set_config({"fail_silently": False, "callbacks": [lambda *args: 1 / 0]}, scope)
         with pytest.raises(ZeroDivisionError):
